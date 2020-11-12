2281680a76df38eafc6d9e12ff0518a9024602ab
Nan <tonynanwork@gmail.com>
2020-11-12 11:45:39 -0800

    hide show edit menu

--- scripts/timestamp.js
+++ scripts/timestamp.js
@@ -1,20 +1,30 @@
 $('#submit').on('click', function (e) {
-            var $textVal = $('#textVal').val();
-            var $listItems = $('.listItems');
-            var dateAdded = new Date();
+    var $textVal = $('#textVal').val();
+    var $listItems = $('.listItems');
+    var dateAdded = new Date();
 
-            $listItems.prepend('<li>' + $textVal + ' added at ' + dateAdded + '</li>');
+    $listItems.prepend('<li>' + $textVal + ' added at ' + dateAdded + '</li>');
 
 
-            $('#textVal').val(' ');
-            db.collection("Posts").add({
-                    "name": $textVal + ' added at ' + dateAdded,
-                })
-                .then(function () {
-                    console.log("Document successfully written!");
-                })
-                .catch(function (error) {
-                    console.error("Error writing document: ", error);
+    $('#textVal').val(' ');
+    db.collection("Posts").add({
+            "name": $textVal + ' added at ' + dateAdded,
+        })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
 
-                });
-            });
+        });
+});
+$('#edit').hide(1000);
+$("#hide").on('click', function (e) {
+    
+    if ($('#edit').is(":visible")){
+        $('#edit').hide(1000);
+    } else {
+        $("#edit").show(1000);
+    }
+
+})
