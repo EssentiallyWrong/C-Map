d35bbe0a07ae00aa0b3cda686620fbca4a9209b8
Nan <tonynanwork@gmail.com>
2020-11-19 13:28:39 -0800

    id

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,18 @@ function initMap() {
     },
     zoom: 13,
   });
+
+  //marker testing
+  const myLatLng = {
+    lat: -25.363,
+    lng: 131.044
+  };
+  const myLat = {
+    lat: +25.363,
+    lng: -131.044
+  };
+
+
   const input = document.getElementById("pac-input");
   const autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.bindTo("bounds", map);
@@ -19,10 +31,48 @@ function initMap() {
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
   const infowindow = new google.maps.InfoWindow();
   const infowindowContent = document.getElementById("infowindow-content");
+
+  var testPlaceId = 'ChIJVVVVVVXlUVMRu-GPNDD5qKw';
+// adding markers from firestore;
+
+  // marker test
+  
+  var service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: testPlaceId
+  }, function (result, status) {
+    var marker = new google.maps.Marker({
+      map: map,
+      place: {
+        placeId: testPlaceId,
+        location: result.geometry.location
+      }
+    });
+  });
+  
+  
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+
+  new google.maps.Marker({
+    position: myLat,
+    map,
+    title: "Another",
+  });
+
   infowindow.setContent(infowindowContent);
+
+
+
   const marker = new google.maps.Marker({
     map: map
   });
+
+
   marker.addListener("click", () => {
     $("#overlay").show(1000);
 
@@ -49,12 +99,20 @@ function initMap() {
     });
     marker.setVisible(true);
     $("#locationName").text(place.name);
+    $("#googlePlaceID").text(place.place_id);
     infowindowContent.children.namedItem("place-name").textContent = place.name;
     infowindowContent.children.namedItem("place-id").textContent =
       place.place_id;
     infowindowContent.children.namedItem("place-address").textContent =
       place.formatted_address;
     infowindow.open(map, marker);
+
+
+
+
   });
 }
+
+
+
 //end of map stuff
